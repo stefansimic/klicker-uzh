55ef67eac4a91dc20dea7329127990be13854c70
Stefan Simic <stefansim2000@gmail.com>
2024-05-08 13:31:45 +0200

    add docker compose of azure

--- _run_app.sh
+++ _run_app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# start postgres, redis, and reverse proxy
-docker compose up --build auth frontend_pwa frontend_manage frontend_control backend
+# start postgres, redis, and reverse proxy, and azure containers
+docker compose up --build auth frontend_pwa frontend_manage frontend_control backend func-response-processor func-incoming-response
