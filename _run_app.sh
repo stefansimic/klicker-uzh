14db4dfeb8ab3cc1e773572ed224d9eb0244747c
root <root@productive-klicker-debian-instance.novalocal>
2024-05-11 10:51:09 +0000

    add .dockerignore

--- _run_app.sh
+++ _run_app.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # start postgres, redis, and reverse proxy, and azure containers
-docker compose up --build auth frontend_pwa frontend_manage frontend_control backend func-response-processor func-incoming-response
+docker compose up --build auth frontend_pwa frontend_manage frontend_control backend func-response-processor func-incoming-responses
